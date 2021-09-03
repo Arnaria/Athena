132fe6b42576780846ac00382e3b7ade3d3cf73e
Ethan <Mr.NavaStar@gmail.com>
2021-09-03 13:11:00 -0600

    Setup

---
+++ src/main/java/arnaria/kingdoms/Kingdoms.java
@@ -0,0 +1,10 @@
+package arnaria.kingdoms;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Kingdoms implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
