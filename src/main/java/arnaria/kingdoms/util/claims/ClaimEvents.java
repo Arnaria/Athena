cc599a9166f50a23c97a09741bb4def09e3ee6c8
Ethan Stevenson <Mr.NavaStar@gmail.com>
2021-09-09 14:56:15 -0600

    Fix heads in claims

--- src/main/java/arnaria/kingdoms/util/claims/ClaimEvents.java
+++ src/main/java/arnaria/kingdoms/util/claims/ClaimEvents.java
@@ -3,22 +3,18 @@ package arnaria.kingdoms.util.claims;
 import arnaria.kingdoms.callbacks.BlockPlaceCallback;
 import arnaria.kingdoms.interfaces.PlayerEntityInf;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-import net.minecraft.block.BannerBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.SkullBlock;
-import net.minecraft.block.WitherSkullBlock;
+import net.minecraft.block.*;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+
+import java.util.ArrayList;
 
 public class ClaimEvents {
 
     public static void register() {
         PlayerBlockBreakEvents.BEFORE.register((world, player, pos, state, blockEntity) -> {
-            if (state.getBlock() instanceof SkullBlock){
-                if (state.getBlock().getClass().equals(WitherSkullBlock.class))
-                return true;
-            }
+            if (state.getBlock() instanceof PlayerSkullBlock) return true;
             return ClaimManager.actionAllowedAt(pos, player);
         });
 
