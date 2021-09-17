076242ede1272d2eaa464acba5115136c3f63a2b
Ethan <Mr.NavaStar@gmail.com>
2021-09-17 02:38:57 -0600

    Remove imports

--- src/main/java/arnaria/kingdoms/mixin/BannerBlockMixin.java
+++ src/main/java/arnaria/kingdoms/mixin/BannerBlockMixin.java
@@ -2,10 +2,7 @@ package arnaria.kingdoms.mixin;
 
 import arnaria.kingdoms.interfaces.BannerMarkerInf;
 import net.minecraft.block.BannerBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockWithEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(BannerBlock.class)
 public class BannerBlockMixin implements BannerMarkerInf {
