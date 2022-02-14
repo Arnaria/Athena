123e6f0d477a4db5b411e3c46c924848d56a5eeb
Ethan Stevenson <Mr.NavaStar@gmail.com>
2022-02-13 20:34:32 -0600

    Fix wither

---
+++ src/main/java/corviolis/athena/mixin/WitherSkullEntityMixin.java
@@ -0,0 +1,33 @@
+package corviolis.athena.mixin;
+
+import corviolis.athena.services.claims.ClaimManager;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.projectile.WitherSkullEntity;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WitherSkullEntity.class)
+public class WitherSkullEntityMixin {
+
+    @Inject(method = "onEntityHit", at = @At("HEAD"), cancellable = true)
+    protected void onEntityHit(EntityHitResult entityHitResult, CallbackInfo ci) {
+        if (ClaimManager.claimExistsAt(new ChunkPos(new BlockPos(entityHitResult.getPos())))) {
+            if (entityHitResult.getEntity() instanceof PassiveEntity) {
+                ci.cancel();
+            }
+        }
+    }
+
+    @Inject(method = "onCollision", at = @At("HEAD"), cancellable = true)
+    protected void onCollision(HitResult hitResult, CallbackInfo ci) {
+        if (ClaimManager.claimExistsAt(new ChunkPos(new BlockPos(hitResult.getPos())))) {
+            ci.cancel();
+        }
+    }
+}
